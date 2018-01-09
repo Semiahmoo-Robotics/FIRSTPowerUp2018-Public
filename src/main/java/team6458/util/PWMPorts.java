504ad6269a4b238013f01664832aabd863b2a42d
GQYZ <gavinzheng2000@gmail.com>
2018-01-08 19:19:24 -0800

    More formatting

--- src/main/java/team6458/util/PWMPorts.java
+++ src/main/java/team6458/util/PWMPorts.java
@@ -7,6 +7,8 @@ public final class PWMPorts {
     public static final int LEFT_MOTOR = 0;
     public static final int RIGHT_MOTOR = 1;
 
-    //Prevents instantiation of class
-    private void PWMPorts() {};
+    /**
+     * Prevents instantiation of class
+     */
+    private PWMPorts() {}
 }
