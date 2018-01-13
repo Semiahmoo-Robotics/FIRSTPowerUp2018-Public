94e1e6a5808c3cc2c1d4c8be26506387ce28bba1
chrislo27 <kinchrislo@gmail.com>
2018-01-12 17:26:50 -0800

    Add subsystems + refactor into Drivetrain

--- src/main/java/team6458/SemiRobot.java
+++ src/main/java/team6458/SemiRobot.java
@@ -1,14 +1,12 @@
 package team6458;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import team6458.subsystem.Drivetrain;
 import team6458.util.CameraSetup;
-import team6458.util.PWMPorts;
 import team6458.util.DashboardKeys;
 import team6458.util.PlateAssignment;
 
@@ -22,41 +20,12 @@ public final class SemiRobot extends TimedRobot {
 
     private static final Logger LOGGER = Logger.getLogger(SemiRobot.class.getName());
 
+    // Xbox controller should be connected on port 0
+    private final XboxController controller = new XboxController(0);
     private PlateAssignment plateAssignment = PlateAssignment.ALL_INVALID;
 
-    //Xbox controller should be connected on port 0
-    private final XboxController controller = new XboxController(0);
-
-    private final DifferentialDrive differentialDrive = new DifferentialDrive(new Spark(PWMPorts.LEFT_MOTOR),new Spark(PWMPorts.RIGHT_MOTOR));
-    /**
-     * @return The non-null plate assignment
-     */
-    public PlateAssignment getPlateAssignment() {
-        return plateAssignment;
-    }
-
-    /**
-     * Internal method that updates the plate assignment from the Field Management System.
-     */
-    private void updatePlateAssignmentFromFMS() {
-        boolean isFMSAttached = DriverStation.getInstance().isFMSAttached();
-        String fmsData = DriverStation.getInstance().getGameSpecificMessage();
-        if (fmsData == null) {
-            /*
-            Note: a reference equality check is valid here because ALL_INVALID is the only possible "unknown"
-            constant that is settable in these conditional branches
-             */
-            if (plateAssignment != PlateAssignment.ALL_INVALID) {
-                LOGGER.log(Level.INFO, "Plate assignment set to ALL_INVALID, got null, was " + plateAssignment + " (FMS attached: " + isFMSAttached + ")");
-                plateAssignment = PlateAssignment.ALL_INVALID;
-            }
-        } else {
-            if (!plateAssignment.toString().equals(fmsData)) {
-                LOGGER.log(Level.INFO, String.format("Plate assignment set to %s, was %s", fmsData, plateAssignment.toString()));
-                plateAssignment = new PlateAssignment(fmsData);
-            }
-        }
-    }
+    // Subsystems
+    private Drivetrain drivetrain;
 
     @Override
     public void robotInit() {
@@ -64,6 +33,11 @@ public final class SemiRobot extends TimedRobot {
 
         // Disables any commands that may run
         Scheduler.getInstance().disable();
+
+        // Start up the subsystems
+        {
+            drivetrain = new Drivetrain(this);
+        }
 
         // Setup the default camera and log the result (successful or not)
         if (CameraSetup.setupDefaultCamera()) {
@@ -127,11 +101,55 @@ public final class SemiRobot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        //The arcade drive function of DifferentialDrive accepts speed and rotation, which is given by the Xbox controller
-        differentialDrive.arcadeDrive(controller.getY(), controller.getX());
+        // The arcade drive function of DifferentialDrive accepts speed and rotation, which is given by the Xbox controller
+        getDrivetrain().drive.arcadeDrive(controller.getY(), controller.getX());
     }
 
     @Override
     public void testPeriodic() {
+    }
+
+    // Subsystem getters
+
+    public Drivetrain getDrivetrain() {
+        if (drivetrain == null) {
+            throw new IllegalStateException("Attempt to get drivetrain instance before initialization!");
+        }
+        return drivetrain;
+    }
+
+
+    // Private methods
+
+    /**
+     * Internal method that updates the plate assignment from the Field Management System.
+     */
+    private void updatePlateAssignmentFromFMS() {
+        boolean isFMSAttached = DriverStation.getInstance().isFMSAttached();
+        String fmsData = DriverStation.getInstance().getGameSpecificMessage();
+        if (fmsData == null) {
+            /*
+            Note: a reference equality check is valid here because ALL_INVALID is the only possible "unknown"
+            constant that is settable in these conditional branches
+             */
+            if (plateAssignment != PlateAssignment.ALL_INVALID) {
+                LOGGER.log(Level.INFO, "Plate assignment set to ALL_INVALID, got null, was " + plateAssignment + " (FMS attached: " + isFMSAttached + ")");
+                plateAssignment = PlateAssignment.ALL_INVALID;
+            }
+        } else {
+            if (!plateAssignment.toString().equals(fmsData)) {
+                LOGGER.log(Level.INFO, String.format("Plate assignment set to %s, was %s", fmsData, plateAssignment.toString()));
+                plateAssignment = new PlateAssignment(fmsData);
+            }
+        }
+    }
+
+    // Getters and setters
+
+    /**
+     * @return The non-null plate assignment
+     */
+    public PlateAssignment getPlateAssignment() {
+        return plateAssignment;
     }
 }
---
+++ src/main/java/team6458/subsystem/RobotSubsystem.java
@@ -0,0 +1,26 @@
+package team6458.subsystem;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import team6458.SemiRobot;
+
+/**
+ * This is an abstract class extending {@link edu.wpi.first.wpilibj.command.Subsystem} that has a
+ * {@link team6458.SemiRobot} instance passed in.
+ *
+ * All {@link RobotSubsystem}s should only be instantiated by the same robot instance.
+ */
+public abstract class RobotSubsystem extends Subsystem {
+
+    /**
+     * The immutable robot instance.
+     */
+    public final SemiRobot robot;
+
+    /**
+     * The main constructor.
+     */
+    public RobotSubsystem(SemiRobot robot, String name) {
+        super(name);
+        this.robot = robot;
+    }
+}
