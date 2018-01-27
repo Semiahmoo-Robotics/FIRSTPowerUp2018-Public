67b1eb18c8ccd4314e26ded8f4345e7476d17124
chrislo27 <kinchrislo@gmail.com>
2018-01-26 19:40:39 -0800

    Add log output for gyro calibration

--- src/main/java/team6458/subsystem/Sensors.java
+++ src/main/java/team6458/subsystem/Sensors.java
@@ -4,11 +4,16 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import team6458.SemiRobot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * The sensors subsystem. This has all the UNCOUPLED sensors for the robot: i.e. those that do not already
  * belong to a subsystem, like the motor encoders belonging to the {@link Drivetrain}.
  */
 public final class Sensors extends RobotSubsystem {
+
+    private static final Logger LOGGER = Logger.getLogger(Sensors.class.getName());
 
     public final BuiltInAccelerometer accelerometer;
     public final ADXRS450_Gyro gyro;
@@ -17,7 +22,9 @@ public final class Sensors extends RobotSubsystem {
         super(robot, "Sensors");
 
         accelerometer = new BuiltInAccelerometer();
+        LOGGER.log(Level.INFO, "Calibrating ADXRS250 gyroscope, expect a block");
         gyro = new ADXRS450_Gyro();
+        LOGGER.log(Level.INFO, "Gyroscope calibrated.");
     }
 
     @Override
