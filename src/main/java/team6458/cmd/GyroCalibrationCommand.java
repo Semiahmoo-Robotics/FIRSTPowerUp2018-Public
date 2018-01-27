029f99746f5768bac193506091077e558c94d766
chrislo27 <kinchrislo@gmail.com>
2018-01-27 10:02:08 -0800

    Add gyro calibration command

---
+++ src/main/java/team6458/cmd/GyroCalibrationCommand.java
@@ -0,0 +1,32 @@
+package team6458.cmd;
+
+import team6458.SemiRobot;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Calibrates the gyroscope.
+ */
+public final class GyroCalibrationCommand extends RobotCommand {
+
+    private static final Logger LOGGER = Logger.getLogger(GyroCalibrationCommand.class.getName());
+
+    public GyroCalibrationCommand(SemiRobot robot) {
+        super(robot);
+        setTimeout(6.0);
+    }
+
+    @Override
+    public synchronized void start() {
+        super.start();
+        LOGGER.log(Level.INFO, "Calibrating gyroscope...");
+        robot.getSensors().gyro.calibrate();
+        LOGGER.log(Level.INFO, "Gyroscope calibrated.");
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+}
