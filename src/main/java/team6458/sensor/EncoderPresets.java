c035f6e86b6487c1644ebd5ec44eea0abaca8bc4
chrislo27 <kinchrislo@gmail.com>
2018-01-29 18:15:55 -0800

    Change gear ratio

--- src/main/java/team6458/sensor/EncoderPresets.java
+++ src/main/java/team6458/sensor/EncoderPresets.java
@@ -10,14 +10,14 @@ public final class EncoderPresets {
     /**
      * The encoder data for the <a href="https://www.andymark.com/encoder-p/am-3314a.htm">CIMcoder</a> encoder type.
      *
-     * <p>The gearbox ratio for the motors these CIMcoders are mounted on is 5.95:1.
-     * (The motor spins 5.95 times for every 1 rotation of the wheels.)
+     * <p>The gearbox ratio for the motors these CIMcoders are mounted on is 10.71:1.
+     * (The motor spins 10.71 times for every 1 rotation of the wheels.)
      *
      * <p>The wheels have a diameter of 15.24 cm (6"), which is a circumference of ~47.878 cm.
      * <b>Note that the values are in the instance are in <i>metres</i>.</b>
      */
     public static final EncoderPresets CIMCODER = new EncoderPresets(EncodingType.k2X, 20,
-            (Math.PI * 0.1524) / 5.95);
+            (Math.PI * 0.1524) / 10.71);
 
     /**
      * The sample rate for the encoder.
