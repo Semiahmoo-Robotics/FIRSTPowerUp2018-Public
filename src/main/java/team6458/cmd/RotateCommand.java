47476568420b8b3fb805cc91f7de2166cd9fc60b
GQYZ <34221683+GQYZ@users.noreply.github.com>
2018-02-03 21:47:07 -0800

    Change Desmos link to permalink
    Old link does not display most up to date link.

--- src/main/java/team6458/cmd/RotateCommand.java
+++ src/main/java/team6458/cmd/RotateCommand.java
@@ -17,7 +17,7 @@ public class RotateCommand extends RobotCommand {
      * <p>
      * Max of 0.45, min of 0.2, range of 20 deg starting at 10 deg.
      *
-     * <p>Desmos curve: https://www.desmos.com/calculator/4fpdhdevdn
+     * <p>Desmos curve: https://www.desmos.com/calculator/eyrhbvtzcz
      */
     public static final SpeedGradient DEFAULT_GRADIENT = new SpeedGradient(0.45, 0.2, 20.0, 10.0);
 
