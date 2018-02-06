bd1c469f96a1a8cc47a37dd706a20e3fa7655f53
chrislo27 <kinchrislo@gmail.com>
2018-02-05 21:46:55 -0800

    Fix forward/backwards being determined by wrong axis

--- src/main/java/team6458/OperatorControl.java
+++ src/main/java/team6458/OperatorControl.java
@@ -81,7 +81,7 @@ public final class OperatorControl {
         }
 
         // Initial magnitude and curve using the controller
-        double magnitude = (isRunHeld ? stickX : (stickX * MAX_NOT_RUN_HELD));
+        double magnitude = (isRunHeld ? stickY : (stickY * MAX_NOT_RUN_HELD));
         double curve = stickX;
         double intakeThrottle = Utils.clamp(
                 (-xboxController.getTriggerAxis(Hand.kLeft) + xboxController.getTriggerAxis(Hand.kRight)), -1.0, 1.0);
