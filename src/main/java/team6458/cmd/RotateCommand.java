c690fbd3e9d2cad2752fc26fb4266c90d3d589e4
chrislo27 <kinchrislo@gmail.com>
2018-03-15 18:59:58 -0700

    Increase min speed to avoid possible stall

--- src/main/java/team6458/cmd/RotateCommand.java
+++ src/main/java/team6458/cmd/RotateCommand.java
@@ -19,7 +19,7 @@ public class RotateCommand extends RobotCommand {
      *
      * <p>Desmos curve: https://www.desmos.com/calculator/eyrhbvtzcz
      */
-    public static final SpeedGradient DEFAULT_GRADIENT = new SpeedGradient(0.45, 0.2, 20.0, 10.0);
+    public static final SpeedGradient DEFAULT_GRADIENT = new SpeedGradient(0.45, 0.4, 20.0, 10.0);
 
     public final double headingChange;
     public final SpeedGradient speedGradient;
