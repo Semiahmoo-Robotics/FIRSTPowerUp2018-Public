ef8a47360dcc0285678a1adb1a36727e72baf650
chrislo27 <kinchrislo@gmail.com>
2018-03-17 21:00:35 -0700

    Add PreferenceKeys

---
+++ src/main/java/team6458/util/PreferenceKeys.java
@@ -0,0 +1,12 @@
+package team6458.util;
+
+/**
+ * A utility class that holds string IDs for keys to {@link edu.wpi.first.wpilibj.Preferences}.
+ */
+public final class PreferenceKeys {
+
+    public static final String DRIVE_STRAIGHT_CALIBRATION = "DriveStraightCalibration";
+
+    private PreferenceKeys() {}
+
+}
