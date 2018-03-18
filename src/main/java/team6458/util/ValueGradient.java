a2cc41fa09661cb749dfaa3d859501d1e049ed46
chrislo27 <kinchrislo@gmail.com>
2018-03-18 16:39:14 -0700

    Clarify documentation + rename a field

--- src/main/java/team6458/util/ValueGradient.java
+++ src/main/java/team6458/util/ValueGradient.java
@@ -1,7 +1,8 @@
 package team6458.util;
 
 /**
- * A simple value holder class that holds a floating point value gradient.
+ * A simple value holder class that holds a floating point value gradient. It consists of two values, a minimum and maximum,
+ * and a range at which it linearly interpolates between those two values.
  */
 public final class ValueGradient {
 
@@ -15,24 +16,24 @@ public final class ValueGradient {
     public final double minimum;
     /**
      * The range in units where the {@link #maximum} transitions to the {@link #minimum} linearly.
-     * This is based on the units remaining plus the {@link #rangeOffset}.
+     * This is based on the units remaining plus the {@link #rangeStart}.
      */
     public final double range;
     /**
      * The start of the {@link #range}. Must greater than or equal to zero.
      * The remaining value from zero to this value will always be at the {@link #minimum}.
      */
-    public final double rangeOffset;
+    public final double rangeStart;
 
-    public ValueGradient(double maximum, double minimum, double range, double rangeOffset) {
-        if (maximum < 0 || maximum > 1 || minimum < 0 || minimum > maximum || rangeOffset < 0) {
+    public ValueGradient(double maximum, double minimum, double range, double rangeStart) {
+        if (maximum < 0 || maximum > 1 || minimum < 0 || minimum > maximum || rangeStart < 0) {
             throw new IllegalArgumentException("Invalid parameters passed for value gradient");
         }
 
         this.maximum = maximum;
         this.minimum = minimum;
         this.range = range;
-        this.rangeOffset = rangeOffset;
+        this.rangeStart = rangeStart;
     }
 
     /**
@@ -40,12 +41,12 @@ public final class ValueGradient {
      * @return A value between {@link #minimum} and {@link #maximum} based on the units remaining
      */
     public double interpolate(double unitsRemaining) {
-        if (unitsRemaining < rangeOffset)
+        if (unitsRemaining < rangeStart)
             return minimum;
-        if (unitsRemaining > rangeOffset + range)
+        if (unitsRemaining > rangeStart + range)
             return maximum;
 
-        final double percentage = Utils.clamp((unitsRemaining - rangeOffset) / range, 0.0, 1.0);
+        final double percentage = Utils.clamp((unitsRemaining - rangeStart) / range, 0.0, 1.0);
         return Utils.lerp(minimum,  maximum, percentage);
     }
 }
